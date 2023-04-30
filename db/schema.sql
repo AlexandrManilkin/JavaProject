5a1441868588c610d2aa56bc2355d45b9c1e5b98
Antonov Sergey <antonov.code@gmail.com>
2023-04-30 20:00:46 +0500

    Пример проекта

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@
 create table groups (
     id             integer     auto_increment primary key,
     group_name     varchar(30) not null,
-	formation_date date        not null,
-	constraint uk_groups_group_formation unique(group_name, formation_date)
+    formation_date date        not null,
+    constraint uk_groups_group_formation unique(group_name, formation_date)
 );
 
 -- Таблица студентов
@@ -18,19 +18,19 @@ create table students (
     lastname     varchar(100),
     ser_passport varchar(4)   not null,
     num_passport varchar(6)   not null,
-	group_id     integer      not null,
-	constraint ch_students_ser check(char_length(ser_passport) = 4),
-	constraint ch_students_num check(char_length(num_passport) = 6),
-	constraint uk_students_ser_num unique(ser_passport, num_passport),
-	constraint fk_students_group_id foreign key (group_id) references groups (id)
+    group_id     integer      not null,
+    constraint ch_students_ser check(char_length(ser_passport) = 4),
+    constraint ch_students_num check(char_length(num_passport) = 6),
+    constraint uk_students_ser_num unique(ser_passport, num_passport),
+    constraint fk_students_group_id foreign key (group_id) references groups (id)
 );
 
 -- Таблица экзаменов
 create table exams (
-	id integer          auto_increment primary key,
-	name varchar(200)   not null,
-	exam_date timestamp not null,
-	constraint uk_exams_name_date unique(name, exam_date)
+    id integer          auto_increment primary key,
+    name varchar(200)   not null,
+    exam_date timestamp not null,
+    constraint uk_exams_name_date unique(name, exam_date)
 );
 
 -- Таблица с расписанием экзаменов студентов
@@ -38,7 +38,7 @@ create table student_exams (
     id           integer auto_increment primary key,
     student_id   integer not null,
     exam_id      integer not null,
-	constraint uk_student_exams_student_exam unique(student_id, exam_id),
-	constraint fk_student_exams_student foreign key (student_id) references students (id),
-	constraint fk_student_exams_exam foreign key (exam_id) references exams (id)
+    constraint uk_student_exams_student_exam unique(student_id, exam_id),
+    constraint fk_student_exams_student foreign key (student_id) references students (id),
+    constraint fk_student_exams_exam foreign key (exam_id) references exams (id)
 );
